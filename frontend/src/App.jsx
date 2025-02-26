e444942542df0be8f65debbeb2f33ce861e540f2
dharshi <dharshib.8@gmail.com>
2025-02-26 16:45:09 +0530

    Implement dark mode styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl text-blue-900">Smart Wallet</h1>
-    </>
+    <div className="dark">
+      <h1 className="text-3xl text-blue-900 dark:bg-blue-950 dark:text-mint-500">Smart Wallet</h1>
+    </div>
   )
 }
 
