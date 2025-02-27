95fdbd4dbc26914571cb261a8d0c51d02cfbce8d
dharshi <dharshib.8@gmail.com>
2025-02-27 08:33:40 +0530

    Add routing with React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
+import { BrowserRouter } from "react-router-dom"
+import AppContainer from "./routes/routes"
+
 function App() {
 
   return (
-    <div className="dark">
-      <h1 className="text-3xl text-blue-900 dark:bg-blue-950 dark:text-mint-500">Smart Wallet</h1>
-    </div>
+    <BrowserRouter>
+      <AppContainer />
+    </BrowserRouter>
   )
 }
 
