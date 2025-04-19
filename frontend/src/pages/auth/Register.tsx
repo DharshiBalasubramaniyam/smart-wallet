dd463a00af0fc7823224f04a2e1d93ec92c129ea
dharshib <Dharshib.8@gmail.com>
2025-04-19 11:19:32 +0530

    Add Google login functionality to registration page

--- frontend/src/pages/auth/Register.tsx
+++ frontend/src/pages/auth/Register.tsx
@@ -7,11 +7,12 @@ import { RegistrationInfo } from "../../interfaces/modals";
 import { AuthService } from "../../services/auth/auth.service";
 import LoadingButton from "../../components/LoadingButton";
 import { Link } from "react-router-dom";
+import { useGoogleLogin } from "@react-oauth/google";
 
 function Register() {
     const [inputs, setInputs] = useState<RegistrationInfo>({ username: "", email: "", password: "" })
     const [loading, setLoading] = useState(false);
-    const { register } = AuthService();
+    const { register, loginWithGoogle } = AuthService();
 
     const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
@@ -31,6 +32,14 @@ function Register() {
         await register(inputs)
         setLoading(false);
     }
+
+    const onGoogleLogin = useGoogleLogin({
+        onSuccess: async (tokenResponse: any) => {
+            console.log(tokenResponse);
+            await loginWithGoogle({token: tokenResponse.access_token})
+        },
+        onError: () => toast.error("Registration failed!")
+      });
 
     return (
         <div className="font-main dark">
@@ -67,7 +76,7 @@ function Register() {
                             <hr className="w-full border-border-light-primary dark:border-border-dark-primary" />
                         </div>
 
-                        <button type="button" className='active:scale-98 mb-3 hs-XqwWvWI-3ad3e02c8d29hs-XqwWvWI- w-full hs-XqwWvWI-3ad3e02c8dhs-XqwWvWI- hs-XqwWvWI-8c9b928e0a29hs-XqwWvWI- flex hs-XqwWvWI-8c9b928e0ahs-XqwWvWI- hs-XqwWvWI-8fa3992b2029hs-XqwWvWI- items-center hs-XqwWvWI-8fa3992b20hs-XqwWvWI- hs-XqwWvWI-f9a4827d4d29hs-XqwWvWI- justify-center hs-XqwWvWI-f9a4827d4dhs-XqwWvWI- hs-XqwWvWI-e577dc2bef29hs-XqwWvWI- gap-4 hs-XqwWvWI-e577dc2befhs-XqwWvWI- hs-XqwWvWI-8395fae57b29hs-XqwWvWI- py-3 hs-XqwWvWI-8395fae57bhs-XqwWvWI- hs-XqwWvWI-ccddc7d62629hs-XqwWvWI- px-6 hs-XqwWvWI-ccddc7d626hs-XqwWvWI- hs-XqwWvWI-36ff319a6829hs-XqwWvWI- text-sm hs-XqwWvWI-36ff319a68hs-XqwWvWI- hs-XqwWvWI-6f1bbbe2f729hs-XqwWvWI- tracking-wide hs-XqwWvWI-6f1bbbe2f7hs-XqwWvWI- hs-XqwWvWI-b7c9ae9a9029hs-XqwWvWI- text-text-light-secondary dark:text-text-dark-secondary hs-XqwWvWI-b7c9ae9a90hs-XqwWvWI- hs-XqwWvWI-9ff09b5fd029hs-XqwWvWI- border hs-XqwWvWI-9ff09b5fd0hs-XqwWvWI- hs-XqwWvWI-1b1ab7973029hs-XqwWvWI- border-border-light-primary dark:border-border-dark-primary hs-XqwWvWI-1b1ab79730hs-XqwWvWI- hs-XqwWvWI-d05034dc4d29hs-XqwWvWI- rounded-md hs-XqwWvWI-d05034dc4dhs-XqwWvWI- hs-XqwWvWI-8e263205c629hs-XqwWvWI- bg-bg-light-primary dark:bg-bg-dark-primary hs-XqwWvWI-8e263205c6hs-XqwWvWI- hs-XqwWvWI-8e06a6b0a629hs-XqwWvWI- hover:bg-hover-light-primary dark:hover:bg-hover-dark-primary hs-XqwWvWI-8e06a6b0a6hs-XqwWvWI- hs-XqwWvWI-5d4084d3cd29hs-XqwWvWI- focus:outline-none hs-XqwWvWI-5d4084d3cdhs-XqwWvWI-'>
+                        <button  onClick={() => onGoogleLogin()} type="button" className='active:scale-98 mb-3 hs-XqwWvWI-3ad3e02c8d29hs-XqwWvWI- w-full hs-XqwWvWI-3ad3e02c8dhs-XqwWvWI- hs-XqwWvWI-8c9b928e0a29hs-XqwWvWI- flex hs-XqwWvWI-8c9b928e0ahs-XqwWvWI- hs-XqwWvWI-8fa3992b2029hs-XqwWvWI- items-center hs-XqwWvWI-8fa3992b20hs-XqwWvWI- hs-XqwWvWI-f9a4827d4d29hs-XqwWvWI- justify-center hs-XqwWvWI-f9a4827d4dhs-XqwWvWI- hs-XqwWvWI-e577dc2bef29hs-XqwWvWI- gap-4 hs-XqwWvWI-e577dc2befhs-XqwWvWI- hs-XqwWvWI-8395fae57b29hs-XqwWvWI- py-3 hs-XqwWvWI-8395fae57bhs-XqwWvWI- hs-XqwWvWI-ccddc7d62629hs-XqwWvWI- px-6 hs-XqwWvWI-ccddc7d626hs-XqwWvWI- hs-XqwWvWI-36ff319a6829hs-XqwWvWI- text-sm hs-XqwWvWI-36ff319a68hs-XqwWvWI- hs-XqwWvWI-6f1bbbe2f729hs-XqwWvWI- tracking-wide hs-XqwWvWI-6f1bbbe2f7hs-XqwWvWI- hs-XqwWvWI-b7c9ae9a9029hs-XqwWvWI- text-text-light-secondary dark:text-text-dark-secondary hs-XqwWvWI-b7c9ae9a90hs-XqwWvWI- hs-XqwWvWI-9ff09b5fd029hs-XqwWvWI- border hs-XqwWvWI-9ff09b5fd0hs-XqwWvWI- hs-XqwWvWI-1b1ab7973029hs-XqwWvWI- border-border-light-primary dark:border-border-dark-primary hs-XqwWvWI-1b1ab79730hs-XqwWvWI- hs-XqwWvWI-d05034dc4d29hs-XqwWvWI- rounded-md hs-XqwWvWI-d05034dc4dhs-XqwWvWI- hs-XqwWvWI-8e263205c629hs-XqwWvWI- bg-bg-light-primary dark:bg-bg-dark-primary hs-XqwWvWI-8e263205c6hs-XqwWvWI- hs-XqwWvWI-8e06a6b0a629hs-XqwWvWI- hover:bg-hover-light-primary dark:hover:bg-hover-dark-primary hs-XqwWvWI-8e06a6b0a6hs-XqwWvWI- hs-XqwWvWI-5d4084d3cd29hs-XqwWvWI- focus:outline-none hs-XqwWvWI-5d4084d3cdhs-XqwWvWI-'>
                             <GoogleIcon />
                             Continue with google
                         </button>
